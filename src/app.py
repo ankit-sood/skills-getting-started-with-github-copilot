ce5b8f3d913a45ccf73cb7c2d3cd81a854ff12fa
ankit-sood <soodankit1993@gmail.com>
2025-03-30 08:38:38 +0000

    Fix typo in signup_for_activity function comment

--- src/app.py
+++ src/app.py
@@ -98,7 +98,7 @@ def signup_for_activity(activity_name: str, email: str):
     # Get the specificy activity
     activity = activities[activity_name]
 
-    # validate studen is not already signed up
+    # validate student is not already signed up
     if email in activity["participants"]:
         raise HTTPException(status_code=400, detail="Student already signed up")    
 
